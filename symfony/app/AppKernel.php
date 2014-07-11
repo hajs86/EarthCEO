09eed9d219183e38b7ba509faf3ee084346a0365
hajs <buglamarek@gmail.com>
2014-07-11 19:44:45 +0200

    excelBundle + uploaderBundle + routing configuration

--- symfony/app/AppKernel.php
+++ symfony/app/AppKernel.php
@@ -16,7 +16,13 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            // My bundles
             new Mb\EarthCeoBundle\MbEarthCeoBundle(),
+
+            //3rd party
+            new Liuggio\ExcelBundle\LiuggioExcelBundle(),
+            new Oneup\UploaderBundle\OneupUploaderBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
