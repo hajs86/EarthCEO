8e69a8d025413ddaa8164e06d7b06035e34a31d5
hajs <buglamarek@gmail.com>
2014-07-11 19:44:45 +0200

    excelBundle + uploaderBundle + routing configuration

---
+++ symfony/src/Mb/EarthCeoBundle/EventListener/UploadListener.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mbugla
+ * Date: 11.07.14
+ * Time: 21:28
+ */
+
+namespace Mb\EarthCeoBundle\EventListener;
+
+use Oneup\UploaderBundle\Event\PostPersistEvent;
+
+class UploadListener
+{
+    public function __construct($doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    public function onUpload(PostPersistEvent $event)
+    {
+        /** @var \Symfony\Component\HttpFoundation\File\File $file */
+        $file = $event->getFile();
+        $response = $event->getResponse();
+
+        $response['files'] = [
+            'name'       => $file->getFilename(),
+            'size'       => $file->getSize(),
+            'url'        => $file->getPath() . '/' . $file->getFileInfo(),
+            'deleteUrl'  => 'deleteUrl',
+            'deleteType' => 'DELETE'
+        ];
+    }
+}
